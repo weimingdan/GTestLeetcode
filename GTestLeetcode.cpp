ea12de835412baa0a3e7e33907d06f751f6634e7
weimingdan <1508281800@qq.com>
2018-08-13 17:21:33 +0800

    vscode gtest

---
+++ GTestLeetcode.cpp
@@ -0,0 +1,48 @@
+// GTestLeetcode.cpp : �������̨Ӧ�ó������ڵ㡣
+//
+
+#include "stdafx.h"
+#include "gtest/gtest.h"
+//#include "../MyLeetcode/Crawler.h"
+
+class CrawlerA
+{
+  public:
+	CrawlerA()
+		: isEnd(false)
+	{
+	}
+	~CrawlerA() {}
+	bool getEnd()
+	{
+		return isEnd;
+	}
+	void setEnd(bool val)
+	{
+		isEnd = val;
+	}
+
+  private:
+	bool isEnd;
+};
+
+TEST(CrawlerTest, test1)
+{
+	CrawlerA s;
+	s.setEnd(true);
+	ASSERT_EQ(true, s.getEnd());
+}
+
+TEST(CrawlerTest2, newTest)
+{
+	CrawlerA s;
+	s.setEnd(false);
+	ASSERT_EQ(false, s.getEnd());
+}
+int xx = 0;
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	return 0;
+}
